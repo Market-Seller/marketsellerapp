d826baa186716475d25806f20928970544110a30
unknown <lucasmasilva2@gmail.com>
2023-11-22 11:32:01 -0300

    Tela Inicial

--- app/src/main/java/com/example/marketsellerapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/marketsellerapp/ui/home/HomeFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.marketsellerapp.databinding.FragmentHomeBinding
+import com.example.marketsellerapp.databinding.FragmentPaginainicialBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentPaginainicialBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,13 +25,9 @@ class HomeFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentPaginainicialBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
